d07c2ab9b0c1460d1d5cda2bf40f60d7dab30f1b
Vadim Tsander <v_tsander@wargaming.net>
2015-05-10 12:33:58 +0300

    added __all__ to db

--- raw_sql_migrate/db.py
+++ raw_sql_migrate/db.py
@@ -4,6 +4,14 @@ from psycopg2 import connect, ProgrammingError
 
 from raw_sql_migrate import config as yaml_config
 
+__all__ = (
+    'execute',
+    'migration_history_exists',
+    'get_latest_migration_number',
+    'create_history_table',
+    'write_migration_history',
+)
+
 
 def execute(sql, params=None, return_result=None, commit=False, config=None):
 
