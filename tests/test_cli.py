be03bb640e7be85a949d62417e8dd08dd0be36e1
Vadim Tsander <ts.taiye@live.com>
2015-07-05 15:00:22 +0300

    fixed tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,40 +2,38 @@
 
 from mock import Mock, patch, call
 
-from tests.base import BaseTestCase
+from tests.base import DatabaseTestCase
 
 from raw_sql_migrate.cli import (
     create, status, migrate, STATUS_HEADER_STRING, AFTER_STATUS_HEADER_STRING, NO_MIGRATION_STRING,
 )
-from raw_sql_migrate.helpers import DatabaseHelper
 
 from raw_sql_migrate.helpers import FileSystemHelper
 
 
-class CliTestCase(BaseTestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        cls.config = 'rsm.yaml'
+class CliTestCase(DatabaseTestCase):
 
     def setUp(self):
+        config = 'rsm.yaml'
         self.create_args = Mock()
-        self.create_args.config = self.config
+        self.create_args.config = config
         self.create_args.name = 'initial'
         self.create_args.package = self.python_path_to_test_package
 
         self.status_args = Mock()
-        self.status_args.config = self.config
+        self.status_args.config = config
         self.status_args.package = None
 
         self.migrate_args = Mock()
-        self.migrate_args.config = self.config
+        self.migrate_args.config = config
         self.migrate_args.package = self.python_path_to_test_package
         self.migrate_args.migration_number = None
+        self.patcher = patch('raw_sql_migrate.cli._get_api', new=lambda config_path: self.api)
+        self.patcher.start()
 
     def tearDown(self):
-        DatabaseHelper.drop_history_table()
         super(CliTestCase, self).tearDown()
+        self.patcher.stop()
 
     def test_create(self):
         self.assertFalse(FileSystemHelper.get_migrations_list(self.python_path_to_test_package))
